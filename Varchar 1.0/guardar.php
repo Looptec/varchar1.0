1cca77b2fc43d9ed241cf850e54f451411e81db9
Looptec <45396319+Looptec@users.noreply.github.com>
2018-11-29 17:19:48 -0600

    Varchar 1.0
    Avance de pagina de inicio y avance de pagina de registro de usuarios

---
+++ Varchar 1.0/guardar.php
@@ -0,0 +1,30 @@
+﻿<?php
+	//conectamos Con el servidor
+	$conectar=@mysql_connect('localhost','root','');
+	//verificamos la conexion
+	if(!$conectar){
+		echo"No Se Pudo Conectar Con El Servidor";
+	}else{
+
+		$base=mysql_select_db('registro');
+		if(!$base){
+			echo"No Se Encontro La Base De Datos";			
+		}
+	}
+	//recuperar las variables
+	$Nombre=$_POST['Nombre'];
+	$Contraseña=$_POST['Contraseña'];
+	$Email=$_POST['Email'];
+	//hacemos la sentencia de sql
+	$sql="INSERT INTO datos VALUES('$Nombre',
+								   '$Contraseña',
+								   '$Email')";
+	//ejecutamos la sentencia de sql
+	$ejecutar=mysql_query($sql);
+	//verificamos la ejecucion
+	if(!$ejecutar){
+		echo"Hubo Algun Error";
+	}else{
+		echo"Datos Guardados Correctamente<br><a href='index.html'>Volver</a>";
+	}
+?>
